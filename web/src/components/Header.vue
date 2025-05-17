807ee7db430f5b36abc278c04dc40be57b0dec5b
hazadus <hazadus7@gmail.com>
2025-05-17 04:42:49 +0300

    feat(web): добавить основу Vue-приложения

---
+++ web/src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="p-4 bg-gray-800 text-white">
+    <nav>
+      <ul class="flex space-x-4">
+        <li>
+          <router-link
+            to="/"
+            class="hover:underline"
+            >Главная</router-link
+          >
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script setup lang="ts"></script>
---
+++ web/src/views/Home.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>Тут будет контент главной страницы</div>
+</template>
+<style scoped></style>
